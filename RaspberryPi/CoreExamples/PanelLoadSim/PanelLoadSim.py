07410bc51003889d70efe6713bb2ca8ebfdcbb45
dgerb <daniel.l.gerber@gmail.com>
2024-09-27 14:32:39 -0700

    debug2

--- RaspberryPi/CoreExamples/PanelLoadSim/PanelLoadSim.py
+++ RaspberryPi/CoreExamples/PanelLoadSim/PanelLoadSim.py
@@ -33,11 +33,11 @@ import RPi.GPIO as GPIO
 fileName = 'output.csv'
 panelAddress = 0x08
 
-electronicsHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 lightingHours = [6, 7, 8, 9, 17, 18, 19, 20, 21, 22, 23]
 ventilationHours = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+electronicsHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 refrigerationHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-channelHours = [lightingHours, ventilationHours, refrigerationHours, electronicsHours]
+channelHours = [lightingHours, ventilationHours, electronicsHours, refrigerationHours]
 
 def StringToBytes(val):
     retVal = []
