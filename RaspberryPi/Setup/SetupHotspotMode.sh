400ea6f001c52d209b234c41e487a43bc2fdcf45
dgerb <daniel.l.gerber@gmail.com>
2025-11-03 16:54:03 -0800

    fixed

--- RaspberryPi/Setup/SetupHotspotMode.sh
+++ RaspberryPi/Setup/SetupHotspotMode.sh
@@ -3,7 +3,7 @@
 # Only run this script if you want to configure the Pi as a hotspot, using its WiFi chip to broadcast a network devices can connect to
 
 # Copy in PicrogridHotspot NetworkManager profile file, with pre-configured SSID, password, WiFi mode, interface, autoconnect IP address
-cp ~/Picrogrid/RaspberryPi/Setup/PicrogridHotspot.nmconnection /etc/NetworkManager/system-connections/PicrogridHotspot.nmconnection
+sudo cp ~/Picrogrid/RaspberryPi/Setup/PicrogridHotspot.nmconnection /etc/NetworkManager/system-connections/PicrogridHotspot.nmconnection
 
 # Must deactivate the current wlan0 network in order to start up the new one
 sudo nmcli connection down preconfigured
