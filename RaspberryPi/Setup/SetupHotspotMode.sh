113f1e9ff811b0187212d6bf09d26e06eb19bb9d
dgerb <daniel.l.gerber@gmail.com>
2025-11-03 20:43:34 -0800

    fix

--- RaspberryPi/Setup/SetupHotspotMode.sh
+++ RaspberryPi/Setup/SetupHotspotMode.sh
@@ -5,8 +5,24 @@
 # Copy in PicrogridHotspot NetworkManager profile file, with pre-configured SSID, password, WiFi mode, interface, autoconnect IP address
 sudo cp ~/Picrogrid/RaspberryPi/Setup/PicrogridHotspot.nmconnection /etc/NetworkManager/system-connections/PicrogridHotspot.nmconnection
 
+# Change ownership to root
+sudo chmod 600 /etc/NetworkManager/system-connections/PicrogridHotspot.nmconnection
+sudo chown root:root /etc/NetworkManager/system-connections/PicrogridHotspot.nmconnection
+
+# Inform NetworkManager to reload its connection profiles so it recognizes the newly added file
+sudo nmcli connection reload
+
+# Show connections as a diagnostic
+sudo nmcli connection show
+
 # Must deactivate the current wlan0 network in order to start up the new one
 sudo nmcli connection down preconfigured
 
 # Activate the new hotspot network to wlan0
 sudo nmcli con up PicrogridHotspot ifname wlan0
+
+# Reboot to restart NetworkManager properly
+sudo reboot
+
+# Restore original preconfigured WiFi network with
+    # sudo nmcli con up preconfigured ifname wlan0
